3b2e6c25c49d86d0a2ecc5f51ea57036f51beb53
Mahesh Bhatt <c-bhattm@kapsarc.org>
2018-09-27 10:26:58 +0530

    refactoring code

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,5 @@
 import React, { Fragment} from 'react';
 import userList from '../../data/user.json';
-import { ReactiveBase } from '@appbaseio/reactivesearch';
 import Header from '../../components/Navigation/Header/Header';
 import OpenMenuDrawer from '../../components/Navigation/OpenMenuDrawer/OpenMenuDrawer';
 import './Layout.css';
@@ -36,14 +35,10 @@ class Layout extends React.Component {
 
         return (
             <Fragment>
-                <ReactiveBase
-                 app="good-books-ds"
-                 credentials="nY6NNTZZ6:27b76b9f-18ea-456c-bc5e-3a5263ebc63d">
                  <Header 
                  openMenuClicked = {this.openMeunHandler } 
                  valueChanged = {(value) => this.valueChangedHandler(value) }
                  currentUser = { this.state.user }/>
-                </ReactiveBase>
                 {(this.state.openMenu)?
                  <OpenMenuDrawer
                     open={this.state.openMenu}
