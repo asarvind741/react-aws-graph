ffc7f8dd1ae824cd6af0f4be0b9bc1ce595499ef
Arvind Singh <c-bhattm@kapsarc.org>
2018-10-17 18:37:56 +0530

    css changes

--- src/index.js
+++ src/index.js
@@ -6,13 +6,12 @@ import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Rehydrated } from 'aws-appsync-react';
 import { ApolloProvider } from 'react-apollo';
-import Client from 'aws-appsync';
 import Amplify, { Auth } from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
 import { AUTH_TYPE } from "aws-appsync/lib/link/auth-link";
 import AWSAppSyncClient from "aws-appsync";
 import { Provider} from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/rootReducer';
 import ReduxPromise from 'redux-promise';
 
@@ -23,7 +22,7 @@ Amplify.configure(awsExports);
 
 /* >>>>>>>Apollo Provide to work with aws<<<<<<< */
 
-// const client = new Client ({
+// const client = new AWSAppSyncClient ({
 //     url: AppSync.graphqlEndpoint,
 //     region: AppSync.region,
 //     auth: {
